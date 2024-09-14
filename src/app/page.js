abfa664dee3307dae6e1b4163581189efe12ecc3
Danyal Imran <dan.imran97@gmail.com>
2024-09-14 17:17:13 -0400

    map works, need to adjust based on users location next

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@
 import { Center, ColorModeScript, Flex, Input, Text } from "@chakra-ui/react";
 import theme from "./theme";
 
+import { MapProvider } from "@/providers/map-provider";
+import { MapComponent } from "@/components/Map";
+
 export default function Home() {
     return (
         <div>
@@ -10,6 +13,11 @@ export default function Home() {
                 <Text size={"lg"}>Welcome to CafeQuest</Text>
                 <Input placeholder="enter location here" width={"300px"} height={"30px"}/>
             </Center>
+
+            <MapProvider>
+                    <MapComponent/>
+                </MapProvider>
+
       </div>
     )
   }
