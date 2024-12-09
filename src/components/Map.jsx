4bc771d5aa0953c72791363320c3ab3ff3799866
Danyal Imran <dan.imran97@gmail.com>
2024-12-09 13:44:11 -0500

    cleared out old code

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,39 +1,3 @@
-// "use client";
-
-// import { Center } from "@chakra-ui/react";
-// import Script from "next/script";
-// import { MapContainer, TileLayer, Marker } from "react-leaflet";
-
-// function Map({ userLocation }) {
-//   const defaultPosition = [51.505, -0.09]; // a fallback location in case it doesn't load for user
-
-//   return (
-//     <>
-//       {/* Dynamically load Leaflet JS */}
-//       <Script
-//         src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-//         integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-//         crossOrigin=""
-//       />
-//       <Center>
-//         <MapContainer
-//           center={userLocation || defaultPosition}
-//           zoom={13}
-//           scrollWheelZoom={true}
-//           style={{ height: "500px", width: "100%" }}
-//         >
-//           <TileLayer
-//             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//           />
-//           {userLocation && <Marker position={userLocation} />}
-//         </MapContainer>
-//       </Center>
-//     </>
-//   );
-// }
-
-// export default Map;
 "use client";
 
 import { useEffect } from "react";
