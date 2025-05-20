ff8f8379de3c9187aa303d1fda13746d61e4c042
Dan Imran <dan.imran97@gmail.com>
2025-05-20 19:19:28 -0400

    add sample dataset of cafes

---
+++ src/data/cafes.js
@@ -0,0 +1,25 @@
+const cafes = [
+    {
+        id: 1,
+        name: "Matchstick Coffee",
+        image: "https://unsplash.com/photos/a-room-filled-with-furniture-and-lots-of-windows-sYpq2Fhy4mY",
+        address: "123 Roast Ave, Toronto",
+        description: "Small batch coffee with local pastries"
+    },
+    {
+        id: 2,
+        name: "Third Wave Espresso",
+        image: "https://unsplash.com/photos/a-cup-of-coffee-and-a-cookie-on-a-saucer-YhvnrNOn5mA",
+        address: "456 Brew Ave, Toronto",
+        description: "Minimalist spot with strong brews."
+    },
+    {
+        id: 3,
+        name: "Bean & Gone",
+        image: "https://unsplash.com/photos/white-ceramic-cup-with-saucer-on-brown-wooden-table-3n3mPoGko8g",
+        address: "789 Steam Blvd, Toronto",
+        description: "Relaxed vibe, open late."
+    }
+]
+
+export default cafes;
