bcf0af212a58c30436f49a84c5f289849b5f309f
Dan Imran <dan.imran97@gmail.com>
2025-05-21 15:08:21 -0400

    removed hard coded color values for cafecards so chakra theme switcher can do its thing

--- src/components/CafeCard.jsx
+++ src/components/CafeCard.jsx
@@ -4,13 +4,13 @@ export default function CafeCard({ cafe }) {
   const fallbackImage = "https://plus.unsplash.com/premium_photo-1664970900025-1e3099ca757a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
   return (
-    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" shadow="md" bg="white" _hover={{ shadow: "lg"}} transition="all 0.2s">
+    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" shadow="md" _hover={{ shadow: "lg"}} transition="all 0.2s">
       <Image src={cafe.image || fallbackImage } alt={cafe.name || "Cafe"} objectFit="cover" height="200px" width="100%" fallbackSrc={fallbackImage} />
 
       <Box p={4}>
         <Stack spacing={2}>
-          <Text fontWeight="bold" fontSize="xl">{cafe.name || "Unnamed Cafe"}</Text>
-          <Text color="gray.600" fontSize="sm">{cafe.address || "Address not available"}</Text>
+          <Text fontWeight="bold" fontSize="xl" >{cafe.name || "Unnamed Cafe"}</Text>
+          <Text  fontSize="sm">{cafe.address || "Address not available"}</Text>
           <Text fontSize="sm">{cafe.description || "No description available"}</Text>
           <Badge colorScheme="green" width="fit-content">Open</Badge>
         </Stack>
