c2e81c3b4f37d17555ae8c22fc9470fce6a98407
Dan Imran <dan.imran97@gmail.com>
2025-05-20 20:05:48 -0400

    add firestore and ability to save cafes to users db

--- src/data/cafes.js
+++ src/data/cafes.js
@@ -2,21 +2,21 @@ const cafes = [
     {
         id: 1,
         name: "Matchstick Coffee",
-        image: "https://unsplash.com/photos/a-room-filled-with-furniture-and-lots-of-windows-sYpq2Fhy4mY",
+        image: "https://plus.unsplash.com/premium_photo-1664970900025-1e3099ca757a?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         address: "123 Roast Ave, Toronto",
         description: "Small batch coffee with local pastries"
     },
     {
         id: 2,
         name: "Third Wave Espresso",
-        image: "https://unsplash.com/photos/a-cup-of-coffee-and-a-cookie-on-a-saucer-YhvnrNOn5mA",
+        image: "https://plus.unsplash.com/premium_photo-1669687924558-386bff1a0469?q=80&w=1288&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         address: "456 Brew Ave, Toronto",
         description: "Minimalist spot with strong brews."
     },
     {
         id: 3,
         name: "Bean & Gone",
-        image: "https://unsplash.com/photos/white-ceramic-cup-with-saucer-on-brown-wooden-table-3n3mPoGko8g",
+        image: "https://images.unsplash.com/photo-1593443320739-77f74939d0da?q=80&w=1336&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         address: "789 Steam Blvd, Toronto",
         description: "Relaxed vibe, open late."
     }
