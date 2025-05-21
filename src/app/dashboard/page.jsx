ba1b68b39f15ab5a0865f42c08ad9a681f347c54
Dan Imran <dan.imran97@gmail.com>
2025-05-21 12:07:55 -0400

    add cafe api info/saved cafes to user dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,19 +1,34 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useAuth } from "@/utils/AuthProvider"
 import { useRouter } from "next/navigation"
-import cafes from "@/data/cafes"
+// import cafes from "@/data/cafes" no need for sample data set as cafecards show now
 import CafeCard from "@/components/CafeCard"
 import { SimpleGrid, Box, Heading } from "@chakra-ui/react";
+import {collection, getDocs } from "firebase/firestore";
+import { db } from "@/config/firebase"
 
 export default function DashboardPage() {
     const { user, loading} = useAuth();
     const router = useRouter();
+    const [savedCafes, setSavedCafes] = useState([]);
+
+    useEffect(() => {
+        const fetchSaved = async () => {
+            if (!user) return;
+            const savedRef = collection(db, "users", user.uid, "savedCafes");
+            const snapshot = await getDocs(savedRef);
+            const cafes = snapshot.docs.map(doc => doc.data());
+            setSavedCafes(cafes);
+        }
+
+        fetchSaved()
+    }, [user]);
 
     useEffect(() => {
         if (!loading && !user ) {
-            router.push("login");
+            router.push("/login");
         }
     }, [user, loading]);
 
@@ -29,11 +44,11 @@ export default function DashboardPage() {
 
 
         <Box p={6}>
-            <Heading mb={4}>Featured Cafes</Heading>
-
+            <Heading mb={4}>Your Saved Cafes</Heading>
             <SimpleGrid columns={{ base: 1, md: 2, lg: 3}} spacing={6}>
-                {cafes.map((cafe) => (
-                    <CafeCard key={cafe.id} cafe={cafe} />
+                {savedCafes.map((cafe) => (
+                    <CafeCard 
+                        key={cafe.id} cafe={cafe} />
                 ))}
             </SimpleGrid>
         </Box>
